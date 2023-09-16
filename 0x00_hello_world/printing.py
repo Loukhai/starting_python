f2bf5fbc6af7a4c38c3bfff64bf3ca0164ed5694
loukhai <loukhairabia@gmail.com>
2023-09-16 15:42:05 +0100

    various formatted strings

--- 0x00_hello_world/printing.py
+++ 0x00_hello_world/printing.py
@@ -6,3 +6,11 @@ print(' "Programming is nice \n')
 # combine text and var w/ formatter string
 number = 30
 print(f"more than {number:d} hello")
+number = 23.73
+print(f"more than {number:f} hello")
+
+str = "hello"
+print(3 * str)
+print(str[:3])
+
+print("{} Welcome to Py!".format(str))
