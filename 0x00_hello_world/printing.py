8c8ba8f2686277e0d87f1f48e83fed81603d4a50
loukhai <loukhairabia@gmail.com>
2023-09-16 12:30:13 +0100

    gitignore

--- 0x00_hello_world/printing.py
+++ 0x00_hello_world/printing.py
@@ -1,6 +1,8 @@
 # print("hello world!")
 
 # with escape a double quotes
-print(' "Programming is nice')
+print(' "Programming is nice \n')
 
-
+# combine text amd var w/ formatter string
+number = 30
+print(f"more than {number:d} hello")
