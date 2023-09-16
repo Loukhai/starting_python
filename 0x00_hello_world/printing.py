67e543c391ea801b30dad2773e0bbfa26e25663a
loukhai <loukhairabia@gmail.com>
2023-09-16 12:36:54 +0100

    fix spelling

--- 0x00_hello_world/printing.py
+++ 0x00_hello_world/printing.py
@@ -3,6 +3,6 @@
 # with escape a double quotes
 print(' "Programming is nice \n')
 
-# combine text amd var w/ formatter string
+# combine text and var w/ formatter string
 number = 30
 print(f"more than {number:d} hello")
